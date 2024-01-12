bd1e8f6713fc19be18be38587c8568a207597e81
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-01-11 20:36:16 -0500

    color de text

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-      <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-full h-screen flex justify-center items-center text-white bg-slate-700 text-3xl">
         Hola soy la pagina
       </div>
     
