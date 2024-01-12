5291a02e03879958407d157cb2fb1278be947564
JuanesEsp1 <juanes.espinosa01@gmail.com>
2024-01-11 20:23:56 -0500

    test

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-      <div>
-        HOla soy la pagina
+      <div className="w-full h-screen flex justify-center items-center">
+        Hola soy la pagina
       </div>
     
   )
